d79950883a882ad7b4eac8af9c65652f05e5810a
Kazi  Alam <Kazi  Alam@DESKTOP-Q7JAL1R.fios-router.home>
2020-10-17 00:01:03 -0400

    new

---
+++ AAAA_aPracticeJava/src/Char.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Char {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number: ");
+		int firstNum = sc.nextInt();
+		System.out.println("Enter second number: ");
+		int secondNum = sc.nextInt();
+		int sum = firstNum+secondNum;
+		System.out.println(sum);
+		
+		
+		/*
+		 * char a='A'; char b='b'; System.out.println(a); System.out.println(b);
+		 */
+
+	}
+
+}
